7d61534eea2978abe81b86581dd59e0fa132e1c1
asipe <andy@zbartech.net>
2017-01-09 07:39:18 -0500

    removed test files from script analyser

--- analyze.ps1
+++ analyze.ps1
@@ -1,6 +1,3 @@
 Import-Module .\thirdparty\psscriptanalyzer_bin\PSScriptAnalyzer.psm1 -Force
 
 Invoke-ScriptAnalyzer -Path .\src\HashBrownz -Recurse -ExcludeRule @('PSUseOutputTypeCorrectly')
-
-Invoke-ScriptAnalyzer -Path .\src\HashBrownz.Tests -Recurse -ExcludeRule @('PSUseOutputTypeCorrectly',
-                                                                           'PSUseShouldProcessForStateChangingFunctions')
